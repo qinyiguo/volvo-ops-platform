6fd4e533e5acde2c45d7b8b065a23c8d35d221d3
Bruce Kuo <bruce800606@gmail.com>
2026-03-01 14:16:20 +0800

    Update db.js

--- server/models/db.js
+++ server/models/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.POSTGRES_CONNECTION_STRING || process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
   max: 20,
   idleTimeoutMillis: 30000,
